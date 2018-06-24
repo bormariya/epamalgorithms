a11c654f2b26fe8ef41794550974bdece4ff493d
Alexey <alexeychubarev@gmail.com>
2018-06-24 12:44:54 +0300

    Homework №2 started

--- src/main/java/NewStack.java
+++ src/main/java/NewStack.java
@@ -1,24 +1,53 @@
 public class NewStack {
-    Element element;
+
+    Element lastElement;
+    int size;
+
+    public  NewStack(){
+        this.size = 0;
+    }
+
+    public NewStack push(Object obj){
+        Element newElement = new Element(obj);
+        newElement.setPreviousElement(lastElement);
+        lastElement = newElement;
+        this.size++;
+        return this;
+    }
+
+    public Object pop(){
+        Object forReturn  = lastElement.getValue();
+        lastElement = lastElement.getPreviousElement();
+        size--;
+        return forReturn;
+    }
+
+    public int getSize(){
+        return this.size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
 
     private class Element{
         Object value;
-        Element nextElement;
+        Element previousElement;
+
+        private Element(Object obj){
+            this.value = obj;
+        }
 
         public Object getValue() {
             return value;
         }
 
-        public Element getNextElement() {
-            return nextElement;
+        public Element getPreviousElement() {
+            return previousElement;
         }
 
-        public void setValue(Object value) {
-            this.value = value;
-        }
-
-        public void setNextElement(Element nextElement) {
-            this.nextElement = nextElement;
+        public void setPreviousElement(Element previousElement) {
+            this.previousElement = previousElement;
         }
     }
 }
